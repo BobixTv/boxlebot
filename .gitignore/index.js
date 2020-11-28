42c94fb3b9e821e9dcc0851d659615397ec6b73f
BobixTv <62499619+BobixTv@users.noreply.github.com>
2020-11-28 18:30:00 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+const Command = require('./command/command')
+const Ping = require('./command/ping')
+const Clear = require('./command/clear')
+
+bot.on('message', message => {
+    Ping.parse(message)
+    Clear.parse(message)
+  })
+
+
+bot.login('NzM3MzY3NTUxNDQwNTg0NzY0.Xx8VCw.ptvUgKjjhy7GASN2vkhaqqcDgmo')
+
+bot.on('ready', () => {
+    console.log('This bot is active!');
+  })
+
+
+bot.on("guildMemberAdd", member => {
+    member.createDM().then(channel => {
+      return channel.send("Salut toi :wave:. Nous te souhaitons la bienvenue dans le serveur de Bobix.")
+  })
+})
+bot.on('guildMemberAdd', member => {
+  member.guild.channels.get('782277062819708958').send('Salut **' + member.user.username + "** !" + " Bienvenue sur le serveur officiel de Bobix :tada: !") 
+});
